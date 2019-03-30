195d2ff4cc78d13639b69f7c4a086c98c870e82e
Luysla Dyana <dyana.tavares1@gmail.com>
2019-03-30 18:58:19 -0300

    Criei página de cadastro

--- src/pages/login-firebase/login-firebase.ts
+++ src/pages/login-firebase/login-firebase.ts
@@ -19,12 +19,13 @@ export class LoginFirebasePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public formBuilder: FormBuilder) {
+
     let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-      this.loginForm = this.formBuilder.group({
-        email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
-        senha: ['', [Validators.required, Validators.minLength(6)]]
-      });
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
 
   }
 
