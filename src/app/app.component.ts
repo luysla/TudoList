a68cea4194bd5fa2e7cbe49bdceaacefee160aaa
Luysla Dyana <dyana.tavares1@gmail.com>
2019-04-02 10:23:10 -0300

    Mudei nome do app para TudoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, Config, App } from 'ionic-angular';
+import { Platform, App } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
