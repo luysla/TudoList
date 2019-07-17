e436eade2065ef7606e5ed5b3dc2d32b13cd0fbd
luysla <dyana.tavares1@gmail.com>
2019-07-16 22:50:45 -0400

    Update .credentials.ts

--- src/app/.credentials.ts
+++ src/app/.credentials.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBuZU-HBv-s4N8IQ_lxo2iwPHey45E-xKs",
-  authDomain: "taskapp-c33ae.firebaseapp.com",
-  databaseURL: "https://taskapp-c33ae.firebaseio.com",
-  projectId: "taskapp-c33ae",
-  storageBucket: "taskapp-c33ae.appspot.com",
-  messagingSenderId: "826268326090"
+  
 };
